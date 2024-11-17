18ce37d8e3c6ea19ad92cbbd59c96478c39bc930
Balaji <balaji.m@splenta.com>
2024-11-17 14:20:26 +0530

    Gradiant button changed for primary

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+          'bg-gradient-to-l from-[#99459A] to-[#F15A29] text-white hover:opacity-90',
         destructive:
           'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
